c3a56130addb991221009896463d96ce52ee4a98
Richard Yu <vipzhicheng@gmail.com>
2020-06-10 11:07:29 +0800

    fix: imp hook_component

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,11 +1,17 @@
 import RedisLoader from '../common/RedisLoader'
+const redisLoader = new RedisLoader()
 
 export const hook_hook = {
   redis_connection: 'Define redis connections, used by ioredis'
 }
 
 export const hook_repl = async () => {
-  const redisLoader = new RedisLoader()
+  return {
+    redis: redisLoader
+  }
+}
+
+export const hook_component = async () => {
   return {
     redis: redisLoader
   }
